f273653e8616afdcebbcd038d880c030e71c26cd
Derrick Bowen <derrickbowen@gmail.com>
2013-08-21 11:06:04 -0500

    show page on first time login

---
+++ wp-content/themes/our-new-tenet/functions.php
@@ -0,0 +1,44 @@
+<?php
+
+
+
+/*
+Plugin Name: Peter's Redirect On First Login (user-meta based)
+Description: Standalone functionality to redirect users to a special page on their first login(s)
+Author: Peter
+Version: 1.0
+Author URI: http://www.theblog.ca/wordpress-redirect-first-login
+*/
+
+// Send new users to a special page
+function redirectOnFirstLogin( $redirect_to, $requested_redirect_to, $user ) {
+    // URL to redirect to
+    $redirect_url = '/first-time/';
+    // How many times to redirect the user
+    $num_redirects = 1;
+
+    // If they're on the login page, don't do anything
+    if( !isset( $user->user_login ) ) {
+        return $redirect_to;
+    }
+
+    $key_name = 'redirect_on_first_login';
+    // Third parameter ensures that the result is a string
+    $current_redirect_value = get_user_meta( $user->ID, $key_name, true );
+    if( '' == $current_redirect_value || intval( $current_redirect_value ) < $num_redirects )
+    {
+        if( '' != $current_redirect_value )
+        {
+            $num_redirects = intval( $current_redirect_value ) + 1;
+        }
+        update_user_meta( $user->ID, $key_name, $num_redirects );
+        return $redirect_url;
+    }
+    else
+    {
+        return $redirect_to;
+    }
+}
+
+add_filter( 'login_redirect', 'redirectOnFirstLogin', 10, 3 );
+?>

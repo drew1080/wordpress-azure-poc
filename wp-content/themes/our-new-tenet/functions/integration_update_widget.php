13e31759fd20bb51be5ee6e85c13508f7de45f59
Andrew Cobb <cobb.andrew@gmail.com>
2013-09-16 13:41:03 -0500

    Adding functionality of the integration widget...boom!

--- wp-content/themes/our-new-tenet/functions/integration_update_widget.php
+++ wp-content/themes/our-new-tenet/functions/integration_update_widget.php
@@ -8,21 +8,21 @@
  */
 
 
-add_action( 'widgets_init', 'my_widget' );
+add_action( 'widgets_init', 'integration_update_widget_init' );
 
 
-function my_widget() {
-	register_widget( 'MY_Widget' );
+function integration_update_widget_init() {
+	register_widget( 'integration_update_widget' );
 }
 
-class MY_Widget extends WP_Widget {
+class Integration_Update_Widget extends WP_Widget {
 
-	function MY_Widget() {
-		$widget_ops = array( 'classname' => 'example', 'description' => __('A widget that displays the authors name ', 'example') );
+	function Integration_Update_Widget() {
+		$widget_ops = array( 'classname' => 'integration-update', 'description' => __('A widget that displays Integration Updates', 'integration-update') );
 		
-		$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'example-widget' );
+		$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'integration-update-widget' );
 		
-		$this->WP_Widget( 'example-widget', __('Example Widget', 'example'), $widget_ops, $control_ops );
+		$this->WP_Widget( 'integration-update-widget', __('Integration Update Widget', 'example'), $widget_ops, $control_ops );
 	}
 	
 	function widget( $args, $instance ) {
@@ -30,23 +30,23 @@ class MY_Widget extends WP_Widget {
 
 		//Our variables from the widget settings.
 		$title = apply_filters('widget_title', $instance['title'] );
-		$name = $instance['name'];
-		$show_info = isset( $instance['show_info'] ) ? $instance['show_info'] : false;
+		$integration_update_count = $instance['integration_update_count'];
 
 		echo $before_widget;
 
 		// Display the widget title 
 		if ( $title )
 			echo $before_title . $title . $after_title;
-
-		//Display the name 
-		if ( $name )
-			printf( '<p>' . __('Hey their Sailor! My name is %1$s.', 'example') . '</p>', $name );
-
-		
-		if ( $show_info )
-			printf( $name );
-
+			
+		query_posts('post_type="integration_update"&posts_per_page=' . $integration_update_count . '&orderby=date&order=DESC');
+    if (have_posts()) : 
+    	while (have_posts()) : the_post(); 
+    		the_title('<h3>', '</h3>');	
+    		echo '<h4>' . get_the_date('d-m-Y') . '</h4>';
+    		the_content();
+    	endwhile;
+    endif; 
+    wp_reset_query();
 		
 		echo $after_widget;
 	}
@@ -58,8 +58,7 @@ class MY_Widget extends WP_Widget {
 
 		//Strip tags from title and name to remove HTML 
 		$instance['title'] = strip_tags( $new_instance['title'] );
-		$instance['name'] = strip_tags( $new_instance['name'] );
-		$instance['show_info'] = $new_instance['show_info'];
+		$instance['integration_update_count'] = strip_tags( $new_instance['integration_update_count'] );
 
 		return $instance;
 	}
@@ -68,26 +67,17 @@ class MY_Widget extends WP_Widget {
 	function form( $instance ) {
 
 		//Set up some default widget settings.
-		$defaults = array( 'title' => __('Example', 'example'), 'name' => __('Bilal Shaheen', 'example'), 'show_info' => true );
+		$defaults = array( 'title' => __('', 'integration-update-widget'), 'integration_update_count' => __('5', 'integration-update-widget') );
 		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
 
-		//Widget Title: Text Input.
 		<p>
 			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title:', 'example'); ?></label>
 			<input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:100%;" />
 		</p>
 
-		//Text Input.
 		<p>
-			<label for="<?php echo $this->get_field_id( 'name' ); ?>"><?php _e('Your Name:', 'example'); ?></label>
-			<input id="<?php echo $this->get_field_id( 'name' ); ?>" name="<?php echo $this->get_field_name( 'name' ); ?>" value="<?php echo $instance['name']; ?>" style="width:100%;" />
-		</p>
-
-		
-		//Checkbox.
-		<p>
-			<input class="checkbox" type="checkbox" <?php checked( $instance['show_info'], true ); ?> id="<?php echo $this->get_field_id( 'show_info' ); ?>" name="<?php echo $this->get_field_name( 'show_info' ); ?>" /> 
-			<label for="<?php echo $this->get_field_id( 'show_info' ); ?>"><?php _e('Display info publicly?', 'example'); ?></label>
+			<label for="<?php echo $this->get_field_id( 'integration_update_count' ); ?>"><?php _e('Integration Updates to show:', 'example'); ?></label>
+			<input id="<?php echo $this->get_field_id( 'integration_update_count' ); ?>" name="<?php echo $this->get_field_name( 'integration_update_count' ); ?>" value="<?php echo $instance['integration_update_count']; ?>" style="width:100%;" />
 		</p>
 
 	<?php

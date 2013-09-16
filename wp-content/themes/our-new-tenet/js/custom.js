2b9cc1d12f561188de33009ff6cf021bfb790131
Andrew Cobb <cobb.andrew@gmail.com>
2013-09-16 16:15:48 -0500

    Resolving conflicts with jquery in the home slider

--- wp-content/themes/our-new-tenet/js/custom.js
+++ wp-content/themes/our-new-tenet/js/custom.js
@@ -1,7 +1,7 @@
-$(function() {
-  if ( $('body').hasClass('page-id-3131') ) {
-    if ( $($('.gde-link')[0]).length > 0) {
-      $($('.gde-link')[0]).click( function(e){
+jQuery(function() {
+  if ( jQuery('body').hasClass('page-id-3131') ) {
+    if ( jQuery(jQuery('.gde-link')[0]).length > 0) {
+      jQuery(jQuery('.gde-link')[0]).click( function(e){
         window.open(this.href, '_blank');
         e.preventDefault();
         return false;

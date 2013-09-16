948f7bd8cef65cdc42e179f8695eca5a9d6fcf80
Andrew Cobb <cobb.andrew@gmail.com>
2013-09-16 09:48:19 -0500

    Styling search

--- wp-content/themes/our-new-tenet/searchform.php
+++ wp-content/themes/our-new-tenet/searchform.php
@@ -1,10 +1,10 @@
 <!-- search -->
-<map name="searchbutton">
-  <area shape="circle" coords="165,10,8" alt="Search" href="<?php echo home_url( '/' ); ?>">
-</map>
+<!-- <map name="searchbutton">
+  <area shape="circle" coords="165,10,8" alt="Search" href="">
+</map> -->
 
 <form role="search" class="search-form" method="get" action="<?php echo home_url( '/' ); ?>" role="search">
 	<input class="search-input-box" type="search" name="s" placeholder="Search" usemap="#searchbutton">
-  <!-- <input type="submit" id="searchsubmit" class="search-button"> -->
+  <input type="submit" id="searchsubmit" class="search-button" value="">
 </form>
 <!-- /search -->

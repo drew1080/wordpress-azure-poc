0e58d8dee47dba2b7953fc2e047700af4b8e8bcd
Kevin Toy <toyk008@gmail.com>
2013-09-16 19:39:17 -0500

    styling faq dropdown

--- wp-content/themes/our-new-tenet/functions.php
+++ wp-content/themes/our-new-tenet/functions.php
@@ -261,4 +261,22 @@ function register_ajaxLoop_script() {
 }
 add_action('wp_enqueue_scripts', 'register_ajaxLoop_script');
 
+
+// Toggle
+
+function toggle_content_func( $atts, $content = null ) {
+	extract(shortcode_atts(array(
+		 'title' => '',
+		 'style' => 'list'
+    ), $atts));
+	output;
+	$output .= '<div class="'.$style.'"><p class="trigger"><a href="#">' .$title. '</a></p>';
+	$output .= '<div class="toggle_container"><div class="block">';
+	$output .= do_shortcode($content);
+	$output .= '</div></div></div>';
+
+	return $output;
+}
+add_shortcode('toggle_content', 'toggle_content_func');
+
 ?>

a00941277d35ab454b040e6b00f79aa9d94f4db8
Andrew Cobb <cobb.andrew@gmail.com>
2013-09-23 09:14:26 -0500

    Password field placeholder

--- wp-content/themes/our-new-tenet/js/custom.js
+++ wp-content/themes/our-new-tenet/js/custom.js
@@ -13,4 +13,34 @@ jQuery(function() {
     jQuery("label[for='user_login']").html('<input type="text" name="log" id="user_login" class="input" value="" size="20" placeholder="Last 4 digits of your SSN">');  
     jQuery("label[for='user_pass']").html('<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" placeholder="Date of birth (mmddyyy)">');
   }
+  
+  FauxPlaceholder();
+  function FauxPlaceholder() {
+    if(!ElementSupportAttribute('input','placeholder')) {
+        $("input[placeholder]").each(function() {
+            var $input = $(this);
+            $input.after('<input id="'+$input.attr('id')+'-faux" style="display:none;" type="text" value="' + $input.attr('placeholder') + '" />');
+            var $faux = $('#'+$input.attr('id')+'-faux');
+
+            $faux.show().attr('class', $input.attr('class')).attr('style', $input.attr('style'));
+            $input.hide();
+
+            $faux.focus(function() {
+                $faux.hide();
+                $input.show().focus();
+            });
+
+            $input.blur(function() {
+                if($input.val() === '') {
+                    $input.hide();
+                    $faux.show();
+                }
+            });
+        });
+    }
+  }
+  function ElementSupportAttribute(elm, attr) {
+      var test = document.createElement(elm);
+      return attr in test;
+  }
 });

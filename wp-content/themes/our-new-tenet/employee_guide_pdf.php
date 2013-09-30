e9b14f2e4e5b5773c04377a429accd32f7ccc128
Andrew Cobb <cobb.andrew@gmail.com>
2013-09-30 11:05:28 -0500

    More meta data for pdf download

--- wp-content/themes/our-new-tenet/employee_guide_pdf.php
+++ wp-content/themes/our-new-tenet/employee_guide_pdf.php
@@ -6,8 +6,14 @@ header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime($path)) . ' GMT');
 header('Accept-Ranges: bytes');  // For download resume
 header('Content-Length: ' . filesize($path));  // File size
 header('Content-Encoding: none');
-header('Content-Type: application/pdf');  // Change this mime type if the file is not PDF
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header('Content-Description: File Transfer');
+header('Content-Type: application/octet-stream');
 header('Content-Disposition: attachment; filename=' . $filename);  // Make the browser display the Save As dialog
+ob_clean();
+flush();
 readfile($path);  //this is necessary in order to get it to actually download the file, otherwise it will be 0Kb
 
 ?>

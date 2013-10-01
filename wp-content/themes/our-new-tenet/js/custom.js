c76d4462fb1a94b55b1cf4b57b8adea69fe7be73
Andrew Cobb <cobb.andrew@gmail.com>
2013-09-30 21:34:49 -0500

    Cleaning up more...remove IE9 check

--- wp-content/themes/our-new-tenet/js/custom.js
+++ wp-content/themes/our-new-tenet/js/custom.js
@@ -59,26 +59,13 @@ jQuery(function() {
       var test = document.createElement(elm);
       return attr in test;
   }
-  
-  // var replaceSliderImages = function (){
-  //   jQuery(jQuery(".tp-leftarrow")[0]).hide();
-  //   jQuery(jQuery(".tp-leftarrow")[0]).css("background", "url(/wordpress-azure-poc/wp-content/themes/our-new-tenet/images/large-left-IE.png)");
-  //   jQuery(jQuery(".tp-leftarrow")[0]).addClass("ie8arrow");
-  //   jQuery(jQuery(".tp-leftarrow")[0]).show();
-  // }
-  // 
-  // setInterval(replaceSliderImages, 1000);
-  // 
-
 });
 
 jQuery(window).load(function() {
-  if( jQuery("html").hasClass("ie8") || jQuery("html").hasClass("ie9")) {
+  if( jQuery("html").hasClass("ie8")) {
     if ( jQuery('body').hasClass('home') ) {    
       
-      jQuery("#credits").append("<p>TEMP IE TEST with window load</p>");
       jQuery(jQuery(".tp-leftarrow")[0]).css("backgroundImage", "url(/wp-content/themes/our-new-tenet/images/large-left-IE.png)");  
-      //jQuery(jQuery(".tp-leftarrow")[0]).addClass("ie8arrow");
 
       jQuery(jQuery(".tp-rightarrow")[0]).css("backgroundImage", "url(/wp-content/themes/our-new-tenet/images/large-right-IE.png)");
       

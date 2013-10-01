f77a6efbaca3893d11c5ac71eaa843069c8fca13
Andrew Cobb <cobb.andrew@gmail.com>
2013-09-30 20:34:41 -0500

    Trying with css file

--- wp-content/themes/our-new-tenet/js/custom.js
+++ wp-content/themes/our-new-tenet/js/custom.js
@@ -66,9 +66,11 @@ jQuery(function() {
     if ( jQuery('body').hasClass('home') ) {    
       
       jQuery("#credits").append("<p>TEMP IE TEST</p>");
-      jQuery(jQuery(".tp-leftarrow.default")[0]).css("background", "url(/wp-content/themes/our-new-tenet/images/large-left-IE.png)");  
+      jQuery(jQuery(".tp-leftarrow")[0]).css("background", "url(/wp-content/themes/our-new-tenet/images/large-left-IE.png)");  
+      jQuery(jQuery(".tp-leftarrow")[0]).addClass("ie8arrow");
 
-      jQuery(jQuery(".tp-rightarrow.default")[0]).css("background", "url(/wp-content/themes/our-new-tenet/images/large-right-IE.png)");
+      jQuery(jQuery(".tp-rightarrow")[0]).css("background", "url(/wp-content/themes/our-new-tenet/images/large-right-IE.png)");
+      jQuery(jQuery(".tp-rightarrow.default")[0]).addClass("ie8arrow");
     }
   };
 });

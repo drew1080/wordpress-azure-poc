695b0fb92d74d29dff44b99f2d5eaaac6f3d82ff
connorposke <connor.poske@gmail.com>
2013-10-01 15:51:52 -0500

    removing more pie.php references

--- wp-content/themes/our-new-tenet/header.php
+++ wp-content/themes/our-new-tenet/header.php
@@ -67,8 +67,8 @@ html.ie #breadcrumbs,
 html.ie a.more-link,
 html.ie .gallery .gallery-item img,
 html.ie .gallery .gallery-item img.thumbnail,
-html.ie .widget-container,
-html.ie #author-info {behavior: url("<?php echo get_stylesheet_directory_uri();?>/PIE.php");position: relative;}</style>
+html.ie .widget-container
+</style>
 <![endif]-->
 
 
